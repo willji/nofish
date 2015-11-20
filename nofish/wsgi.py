57e0decf403a3e4560e8c523d675a118f37959b3
will <will@www.nofish.cn>
2015-11-20 16:03:12 +0800

    15.11.20-1

---
+++ nofish/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for nofish project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/1.8/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "nofish.settings")
+
+application = get_wsgi_application()
